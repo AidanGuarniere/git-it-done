7af7894522ac5cf726f4832cc8e3db9d5cd3dd56
aidan <aidanguarniere@gmail.com>
2020-11-11 14:49:09 -0800

    merged to develop

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -7,8 +7,8 @@ let repoSearchTerm = document.querySelector("#repo-search-term");
 
 // find GitHub user repositories based off of user variable
 var getUserRepos = function(user) {
-        // format the github api url
-        var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
         // make a request to the url
         fetch(apiUrl)
             .then(function(response) {
@@ -53,7 +53,7 @@ let displayRepos = function(repos, searchTerm) {
     repoContainerEl.textContent = "No repositories found.";
     return;
     }
-    // clear old content
+    // clear old content 
     repoContainerEl.innerHTML = "";
     // display search term
     repoSearchTerm.textContent = searchTerm;
@@ -92,74 +92,3 @@ let displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
-
-
-
-
-
-
-
-// var repoContainerEl = document.querySelector('#repos-container');
-// var repoSearchTerm = document.querySelector('#repo-search-term');
-// var userFormEl =document.querySelector('#user-form');
-// var nameInputEl=document.querySelector('#username');
-// var displayRepos = function(repos, searchTerm){
-//   console.log(repos);
-//   console.log(searchTerm);
-//   //clear old content
-//   repoContainerEl.textContent = '';
-//   repoSearchTerm.textContent = searchTerm;
-//   //loop over repos
-//   for(var i=0; i < repos.length; i++){
-//     //format repo name
-//     var repoName = repos[i].owner.login + '/' + repos[i].name;
-//     //create a container for each repo
-//     var repoEl = document.createElement('div');
-//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
-//     //create a span element to hold repository name
-//     var titleEl = document.createElement('span');
-//     titleEl.textContent = repoName;
-//     //append to container
-//     repoEl.appendChild(titleEl);
-//     //append container to the dom
-//     repoContainerEl.appendChild(repoEl);
-//   }
-// }
-// var formSubmitHandler = function(event){
-//   event.preventDefault();
-//   //get value from input element
-//   var username = nameInputEl.value.trim();
-//   if(username){
-//     getUserRepos(username);
-//     nameInputEl.value
-//   } else{
-//     alert('please enter a GitHub username');
-//   }
-//   console.log(event)
-// };
-// var getUserRepos = function(user) {
-//     // format the github api url
-//     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-//     // make a request to the url
-//     fetch(apiUrl).then(function(response) {
-//       return(response.json());
-//     })
-//     .then(function(data) {
-//       displayRepos(data, user);
-//     });
-//   };
-// userFormEl.addEventListener('submit', formSubmitHandler);
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
